ed6908d51135f92313a3fb67e9b6003a0bdeda9e
fmagmych <mychaelmagnuson@gmail.com>
2023-06-13 11:36:49 -0500

    Adding data.js for object data to work with; app.jsx updated with functioning useState methods to display the puppies and to show additional details for each puppy when they are clicked on

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { puppyList } from "./data.js";
+import { useState } from "react";
 import "./App.css";
+// import "./path-to-css.css";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
-      <div></div>
+      <div className="App">
+        {puppies.map((puppy) => {
+          // eslint-disable-next-line react/jsx-key
+          return (
+            <p
+              onClick={() => {
+                console.log("puppy id: ", puppy.id);
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        <div>
+          {featPupId && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        ;
+      </div>
     </>
   );
 }
